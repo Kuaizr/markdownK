231db5ba389e3923c06a81b13baf2b2a30209cfc
kzer <kuaizhirui@gmail.com>
2023-01-24 21:14:37 +0800

    fix:开屏亮屏的错误

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,10 @@ import * as vscode from 'vscode';
 import { autoClearCacheStorage } from './service/autoClearCacheStorage';
 import { MarkdownEditorProvider } from './provider/markdownEditorProvider';
 import { MarkdownService } from './service/markdownService';
-// const httpExt:any = require('./bundle/extension.js');
 
 
 export function activate(context: vscode.ExtensionContext) {
-
-	// httpExt.activate(context);
 	autoClearCacheStorage();
-
 	const viewOption = { webviewOptions: { retainContextWhenHidden: true, enableFindWidget: true } };
 	const markdownService = new MarkdownService(context);
 	context.subscriptions.push(
